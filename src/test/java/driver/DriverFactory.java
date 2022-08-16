e56397de7ebc4ba68477e7bb2a25b380f4ac79bd
Tran Thao Quyen <tthaoquyen01995@gmail.com>
2022-08-16 11:00:05 +0700

    lession-22

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -11,14 +11,17 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory implements MobileCapabilityTypeEx {
+
+    private AppiumDriver<MobileElement> appiumDriver;
+
     public static AppiumDriver<MobileElement> getDriver(Platform platform) {
         AppiumDriver<MobileElement> appiumDriver = null;
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(PLATFORM_NAME, "Android");
         desiredCapabilities.setCapability(AUTOMATION_NAME, "uiautomator2");
-        desiredCapabilities.setCapability(UDID, "192.168.52.103:5555");
+        desiredCapabilities.setCapability(UDID, "3300d3672cca62b9");
         desiredCapabilities.setCapability(APP_PACKAGE, "com.wdiodemoapp");
-        desiredCapabilities.setCapability(APP_ACTIVITY, ".MainActivity t7");
+        desiredCapabilities.setCapability(APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
         URL appiumServer = null;
 
         try {
@@ -40,8 +43,51 @@ public class DriverFactory implements MobileCapabilityTypeEx {
         }
 
         // Implicit wait | Interval time 500ms
-        appiumDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        appiumDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         return appiumDriver;
+    }
+
+    public AppiumDriver<MobileElement> getDriver(Platform platform, String udid, String systemPort) {
+        if(appiumDriver == null) {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(PLATFORM_NAME, "Android");
+            desiredCapabilities.setCapability(AUTOMATION_NAME, "uiautomator2");
+            desiredCapabilities.setCapability(UDID, udid);
+            desiredCapabilities.setCapability(APP_PACKAGE, "com.wdiodemoapp");
+            desiredCapabilities.setCapability(APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
+            desiredCapabilities.setCapability(SYSTEM_PORT, systemPort);
+            URL appiumServer = null;
+
+            try {
+                appiumServer = new URL("http://localhost:4723/wd/hub");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (appiumServer == null)
+                throw new RuntimeException("Can't construct the appium server url @http://localhost:4723/wd/hub");
+
+            switch (platform) {
+                case ANDROID:
+                    appiumDriver = new AndroidDriver<>(appiumServer, desiredCapabilities);
+                    break;
+                case IOS:
+                    appiumDriver = new IOSDriver<>(appiumServer, desiredCapabilities);
+                    break;
+            }
+
+            // Implicit wait | Interval time 500ms
+            appiumDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        }
+
+        return appiumDriver;
+    }
+
+    public void quitAppiumDriver(){
+        if(appiumDriver != null){
+            appiumDriver.quit();
+            appiumDriver = null;
+        }
     }
 }
--- src/test/java/models/components/global/BottomNavComponent.java
+++ src/test/java/models/components/global/BottomNavComponent.java
@@ -4,9 +4,12 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
+
 public class BottomNavComponent {
+
     private final AppiumDriver<MobileElement> appiumDriver;
     private final static By loginIconSel = MobileBy.AccessibilityId("Login");
+    private final static By formsIconSel = MobileBy.AccessibilityId("Forms");
 
     public BottomNavComponent(AppiumDriver<MobileElement> appiumDriver) {
         this.appiumDriver = appiumDriver;
@@ -14,5 +17,9 @@ public class BottomNavComponent {
 
     public void clickOnLoginIcon(){
         appiumDriver.findElement(loginIconSel).click();
+    }
+
+    public void clickOnFormsIcon() {
+        appiumDriver.findElement(formsIconSel).click();
     }
 }
